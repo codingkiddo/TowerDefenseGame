629da707ee108a016261e565d64a65466c64a694
faresbenamor <48929657+faresbenamor@users.noreply.github.com>
2020-05-05 02:00:51 +0100

    Missile

--- New Unity Project (2)/Assets/Scripts/Bullet.cs
+++ New Unity Project (2)/Assets/Scripts/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
     public void Seek(Transform _target) {
         target = _target;
@@ -30,14 +31,50 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame,Space.World);
-         
+        transform.LookAt(target);
     }
 
     void HitTarget() {
         GameObject effectIns = (GameObject)Instantiate(impactEffect,transform.position,transform.rotation);
-        Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        Destroy(effectIns, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        } 
+        
+        else
+        
+        {
+            Damage(target);
+        }
+
+       
         Destroy(gameObject);
+    }
+
+    void Explode ()
+    {
+       Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider collider in colliders )
+        {
+            if ( collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 
 }
