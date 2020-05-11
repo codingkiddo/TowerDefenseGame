033ded78b893f936334b9e4ace90da142b5eed34
faresbenamor <48929657+faresbenamor@users.noreply.github.com>
2020-05-11 01:25:34 +0100

    Laser Beamer

--- New Unity Project (2)/Assets/Scripts/Shop.cs
+++ New Unity Project (2)/Assets/Scripts/Shop.cs
@@ -4,6 +4,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint standardTurret;
     public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
     BuildManager buildManager;
 
      void Start()
@@ -21,6 +22,12 @@ public class Shop : MonoBehaviour
     {
         Debug.Log("Missile Launcher Selected");
         buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    public void SelectLaserBeamer()
+    {
+        Debug.Log("Laser Beamer Selected");
+        buildManager.SelectTurretToBuild(laserBeamer);
     }
 
 }
